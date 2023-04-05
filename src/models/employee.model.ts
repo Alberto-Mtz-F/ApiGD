4401304e5fc1d2ecb4f4629b4c748ce40b0be474
Alberto Mtz <jalberto.mf21@gmail.com>
2023-04-04 19:27:17 -0700

    validation for user,employee and role

--- src/models/employee.model.ts
+++ src/models/employee.model.ts
@@ -1,19 +1,28 @@
+import { IsString, IsDateString, IsEmail, IsNumberString, IsStrongPassword, IsNumber, IsArray } from "class-validator";
 import { IUser } from "./user.model";
 
-export interface IEmployee{
+export class IEmployee{
+    @IsString()
     name : string;
 
+    @IsString()
     fristSurname : string;
-
+    
+    @IsString()
     secondSurname : string;
     
+    @IsDateString()
     birthday: string;
 
+    @IsEmail()
     email: string;
 
+    @IsNumberString()
     phonenumber: string;
 
+    @IsString()
     status: string;
-
-    user: IUser
+    
+    user:IUser // No se puede validar (Desde el Thunder Client al menos no logro apreciar como validar)
+        
 }
--- src/models/role.model.ts
+++ src/models/role.model.ts
@@ -1,4 +1,7 @@
-export interface IRole{
+import { IsString } from "class-validator";
+
+export class IRole{
+    @IsString()
     name: string;
     
 }
