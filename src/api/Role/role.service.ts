fef96d48d09620fdc320b8b946a0a7ad1e2fc173
Alberto Mtz <jalberto.mf21@gmail.com>
2023-05-08 13:05:43 -0700

    get by UUID in User and documentation from Employee, User and Role

--- src/api/Role/role.service.ts
+++ src/api/Role/role.service.ts
@@ -13,16 +13,41 @@ export class RoleService {
         private userService: UserService
     ){}
 
+    /**
+     * This function creates a new role entity in a database using the provided role object.
+     * @param {IRole} role - IRole is likely an interface or type that defines the shape of an object
+     * representing a role in the system. It could include properties such as the name of the role, its
+     * permissions, and any other relevant information. The `create` function appears to be an
+     * asynchronous method that takes in an object of
+     * @returns The `create` method is returning a Promise that resolves to the result of inserting the
+     * `role` object into the `roleEntity`. The result could be the newly created `role` object or some
+     * other value depending on the implementation of the `insert` method.
+     */
     async create (role: IRole){
         return await this.roleEntity.insert(role);
     }
 
+    /**
+     * This function returns all role entities with their associated user entities.
+     * @returns The `getAll()` function is returning an array of `roleEntity` objects with their
+     * associated `user` objects included as a relation.
+     */
     getAll(){
         return this.roleEntity.find({
             relations:['user'],
         })
     }
 
+    /**
+     * This function retrieves a role by its ID and validates its existence before returning it along
+     * with its associated users.
+     * @param {number} id_role - The parameter `id_role` is a number that represents the ID of a role
+     * that we want to retrieve from the database.
+     * @returns The function `getbyID` returns a Promise that resolves to a single `Role` entity with
+     * its associated `User` entities, based on the provided `id_role` parameter. Before returning the
+     * entity, the function first checks if the `Role` entity exists and validates it using the
+     * `validateRole` function.
+     */
     async getbyID(id_role: number){
         const roleExist = await this.roleEntity.findOne({where:{id:id_role}})
         this.validateRole(roleExist, id_role)
@@ -32,6 +57,15 @@ export class RoleService {
         })
     }
     
+    /**
+     * This function updates a role by its ID in a database using TypeScript.
+     * @param {number} id - The ID of the role that needs to be updated in the database.
+     * @param {IRole} role - The `role` parameter is an object of type `IRole` which contains
+     * information about the role that needs to be updated. It is passed as an argument to the
+     * `updateRolebyID` function.
+     * @returns The `updateRolebyID` function is returning a promise that resolves to the result of
+     * updating the role with the specified `id` with the new `role` object.
+     */
     async updateRolebyID(id: number, role: IRole){
         const roleExist = await this.roleEntity.findOne({where:{id:id}})
         this.validateRole(roleExist, id)
@@ -39,6 +73,13 @@ export class RoleService {
         return await this.roleEntity.update({id}, role)
     }
 
+    /**
+     * This function deletes a role by its ID and also deletes all users associated with that role.
+     * @param {number} id - The ID of the role that needs to be deleted.
+     * @returns a Promise that resolves to the result of deleting a role with the specified ID from the
+     * database. If the role does not exist, it returns false. Before deleting the role, it also
+     * deletes all users associated with that role.
+     */
     async deleteRole(id: number){
         const roleExist = await this.getbyID(id)
         if(!roleExist) return false
@@ -46,6 +87,11 @@ export class RoleService {
         return await this.roleEntity.delete({id})
     }
 
+    /**
+     * This function deletes all users associated with a given role.
+     * @param {Role} roleExist - Role object that contains information about a specific role, including
+     * the users assigned to that role.
+     */
     async deleteusers(roleExist: Role){
         let user = roleExist.user
         for (let index = 0; index < user.length; index++) {
@@ -53,6 +99,13 @@ export class RoleService {
         }
     }
 
+    /**
+     * This function validates if a role exists based on its ID and logs an error message if it
+     * doesn't.
+     * @param {Role} roleExist - A variable that represents whether a role exists or not. It is likely
+     * a boolean value.
+     * @param {number} id_role - The id of the role that needs to be validated.
+     */
     validateRole(roleExist: Role, id_role: number){
         if(!roleExist){
             console.error(`No se a encontrado al rol con id ${id_role}`)
--- src/api/User/user.service.ts
+++ src/api/User/user.service.ts
@@ -17,6 +17,16 @@ export class UserService {
         
     ){}
 
+    /**
+     * This function creates a new user in the database after checking if the role and employee exist
+     * and if the employee already has a user associated with them.
+     * @param {IUser} user - an object representing a user to be created, with properties such as name,
+     * email, password, role, and employee.
+     * @returns the result of the `insert` method of the `userEntity` object, which is likely a Promise
+     * that resolves to the newly created user object. However, if any of the conditions in the
+     * function are not met, it will return an error message using `console.error` instead of the user
+     * object.
+     */
     async create(user: IUser){
         const rolexist = await this.roleEntity.findOne({where:{id: user.role}})
         if (!rolexist) return console.error(`No existe rol con id ${user.role}`)
@@ -27,10 +37,26 @@ export class UserService {
         return await this.userEntity.insert(user)
     }
 
+    /**
+     * This function retrieves all user entities with their related role and employee entities.
+     * @returns The `getAll()` function is returning all the user entities from the database along with
+     * their related `role` and `employee` entities.
+     */
     getAll(){
         return this.userEntity.find({relations: ['role', 'employee']})
     }
 
+    /**
+     * This function retrieves a user by their ID, including their role and employee relations, after
+     * validating their existence.
+     * @param {number} id_user - The parameter `id_user` is a number that represents the unique
+     * identifier of a user in the database. This function uses this parameter to find a user in the
+     * database and return their information along with their associated role and employee data.
+     * @returns The function `getbyID` returns a Promise that resolves to a user object with its
+     * related role and employee data, if a user with the specified `id_user` exists in the database.
+     * Before returning the user object, the function first validates if the user exists by calling the
+     * `validateUser` function.
+     */
     async getbyID(id_user: number){
         const userExist = await this.userEntity.findOne({where:{id:id_user}})
         this.validateUser(userExist, id_user)
@@ -40,7 +66,35 @@ export class UserService {
             
         })
     }
+
+    /**
+     * This function retrieves a user by their UUID and returns their information along with their
+     * associated role and employee data.
+     * @param {string} uuid_user - The parameter `uuid_user` is a string that represents the UUID
+     * (Universally Unique Identifier) of a user. It is used to search for a user in the database and
+     * retrieve their information, including their role and employee details.
+     * @returns a user entity with its related role and employee entities, based on the provided UUID.
+     * Before returning the user entity, the function first checks if the user exists and is valid.
+     */
+    async getbyUUID(uuid_user: string){
+        const userExist = await this.userEntity.findOne({where:{uuid:uuid_user}})
+        this.validateUser(userExist, userExist.id)
+        return await this.userEntity.findOne({
+            relations: ['role', 'employee'],
+            where: {uuid: uuid_user}
+            
+        })
+    }
     
+    /**
+     * This function updates a user by their ID after validating their existence.
+     * @param {number} id - The ID of the user that needs to be updated in the database.
+     * @param {IUser} user - IUser is likely an interface or type definition for a user object, which
+     * may include properties such as name, email, password, and other relevant information. This
+     * parameter is used to update an existing user in the database with the provided information.
+     * @returns The `updateUserbyID` function is returning a Promise that resolves to the result of
+     * updating the user with the given `id` with the properties of the `user` object.
+     */
     async updateUserbyID(id: number, user: IUser){
         const userExist = await this.userEntity.findOne({where:{id:id}})
         this.validateUser(userExist, id)
@@ -48,6 +102,14 @@ export class UserService {
         return await this.userEntity.update({id}, user)
     }
 
+    /**
+     * This is an asynchronous function that deletes a user by their ID if they exist.
+     * @param {number} id - The id parameter is a number that represents the unique identifier of the
+     * user that needs to be deleted.
+     * @returns The `deleteUser` function is returning a Promise that resolves to either `false` if the
+     * user with the given `id` does not exist, or the result of deleting the user with the given `id`
+     * from the `userEntity`.
+     */
     async deleteUser(id: number){
         const userExist = await this.getbyID(id)
         if(!userExist) return false
@@ -55,6 +117,12 @@ export class UserService {
         return await this.userEntity.delete({id})
     }
 
+    /**
+     * The function validates if a user exists and logs an error message if not.
+     * @param {User} userExist - A variable that represents whether a user exists or not. It is likely
+     * a boolean value.
+     * @param {number} id_user - The id of the user that needs to be validated.
+     */
     validateUser(userExist: User, id_user: number){
         if(!userExist){
             console.error(`No se a encontrado al usuario con id ${id_user}`)
